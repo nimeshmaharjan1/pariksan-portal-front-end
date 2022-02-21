32ffa3b0d7d86c7c3ccc32f142844e4dc388f9e7
Nimesh Maharjan <maharjannimesh11@gmail.com>
2022-02-21 23:42:53 +0545

    update question

---
+++ src/app/pages/admin/question/update-question/update-question.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-question',
+  templateUrl: './update-question.component.html',
+  styleUrls: ['./update-question.component.scss']
+})
+export class UpdateQuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
